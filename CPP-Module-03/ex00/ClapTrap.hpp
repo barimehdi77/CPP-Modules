2179d9e2aaa1a0cf4cea817b56e8d48627b0e6ee
barimehdi77 <Barimehdi777@gmail.com>
2021-09-17 19:39:31 +0100

    start working on ex00 in module 03

---
+++ CPP-Module-03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 18:45:10 by mbari             #+#    #+#             */
+/*   Updated: 2021/09/17 19:06:48 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string	_Name;
+		int			_Hitpoint;
+		int			_EnergyPoints;
+		int			_AttackDamage;
+	public:
+		ClapTrap();
+		ClapTrap( std::string );
+		ClapTrap( const ClapTrap &src );
+		~ClapTrap();
+		ClapTrap & operator = ( const ClapTrap &rhs );
+		void	attack( std::string const & target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
+};
+
+#endif
--- CPP-Module-03/ex00/main.cpp
+++ CPP-Module-03/ex00/main.cpp
@@ -6,7 +6,23 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 16:55:47 by mbari             #+#    #+#             */
-/*   Updated: 2021/09/16 16:55:48 by mbari            ###   ########.fr       */
+/*   Updated: 2021/09/17 19:32:13 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+
+int main( void )
+{
+	ClapTrap mehdi("mehdi");
+	ClapTrap ayoub("ayoub");
+
+	ayoub.beRepaired(10);
+	mehdi.attack("ayoub");
+	ayoub.takeDamage(10);
+	mehdi.beRepaired(10);
+	ayoub.attack("mehdi");
+	mehdi.takeDamage(10);
+	return (0);
+}
