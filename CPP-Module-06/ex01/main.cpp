a89a85ba45f70bcfbdd2855437a9704d763164be
barimehdi77 <Barimehdi777@gmail.com>
2021-11-06 12:07:37 +0100

    add serialize and deserialize function

--- CPP-Module-06/ex01/main.cpp
+++ CPP-Module-06/ex01/main.cpp
@@ -6,50 +6,54 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 17:42:51 by mbari             #+#    #+#             */
-/*   Updated: 2021/11/05 13:14:37 by mbari            ###   ########.fr       */
+/*   Updated: 2021/11/06 12:05:24 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <cstdint>
-#include <iomanip>
-#include <type_traits>
-#include "headers/Casts.hpp"
 
-int main(int ac, char **av)
+struct Data
 {
-	try
-	{
-		if (ac == 2)
-		{
-			Casts Number(av[1]);
-			if (Number.getType() == 'i')
-				Number.FromInt();
-			else if (Number.getType() == 'f')
-				Number.FromFloat();
-			else if (Number.getType() == 'c')
-				Number.FromChar();
-			else if (Number.getType() == 'd')
-				Number.FromDouble();
-			std::cout << "Type: " << Number.getType() << std::endl;
-			std::cout << "Char: " << Number.print('c', av[1]) << std::endl;
-			std::cout << "Int: " << Number.print('i', av[1]) << std::endl;
-			// std::cout << std::fixed;
-			// std::cout << std::setprecision(1);
-			std::cout << "Float: " << Number.print('f', av[1]) << std::endl;
-			std::cout << "Double: " << Number.print('d', av[1]) << std::endl;
-		}
-		else
-		{
-			std::cout << "Error: Enter one argument!" << std::endl;
-		}
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	int		i1;
+	int		i2;
+	char	c;
+	bool	b;
+};
+
+uintptr_t serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
+
+int main()
+{
+	Data data;
+	Data *ptr;
+	uintptr_t raw;
+
+	data.i1 = 7;
+	data.i2 = 77;
+	data.c = 'M';
+	data.b = true;
+
+	std::cout << data.i1 << std::endl;
+	std::cout << data.i2 << std::endl;
+	std::cout << data.c << std::endl;
+	std::cout << data.b << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	raw = serialize(&data);
+	ptr = deserialize(raw);
+	std::cout << ptr->i1 << std::endl;
+	std::cout << ptr->i2 << std::endl;
+	std::cout << ptr->c << std::endl;
+	std::cout << ptr->b << std::endl;
+
 
 	return (0);
 }
