48c8d82f4197aa53a1b4d034fecccbb49de1feac
barimehdi77 <Barimehdi777@gmail.com>
2021-11-10 15:32:41 +0100

    add swap, min and max templates to whatever.hpp; add some test in main.cpp

--- CPP-Module-07/ex00/main.cpp
+++ CPP-Module-07/ex00/main.cpp
@@ -6,7 +6,25 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 18:28:01 by mbari             #+#    #+#             */
-/*   Updated: 2021/11/08 18:28:02 by mbari            ###   ########.fr       */
+/*   Updated: 2021/11/10 15:27:32 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ CPP-Module-07/ex00/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 15:12:34 by mbari             #+#    #+#             */
+/*   Updated: 2021/11/10 15:31:21 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void	swap(T &x, T &y)
+{
+	T tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <typename T>
+T	min(T x, T y)
+{
+	return ((x < y) ? x : y);
+}
+
+template <typename T>
+T	max(T x, T y)
+{
+	return ((x > y) ? x : y);
+}
+#endif
